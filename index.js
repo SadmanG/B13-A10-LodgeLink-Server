d42a1c5432a6938dfbe2e44d1f73bdc7026b0191
SadmanG <sadmangergees@gmail.com>
2026-06-26 15:38:35 +0600

    All Properties Page connected with MongoDB

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const app = express()
+const cors = require('cors');
 const port = 5000
+
+app.use(cors());
+app.use(express.json());
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const uri = "mongodb+srv://SadmanG:<db_password>@dragon-news-sadg.vr3snic.mongodb.net/?appName=Dragon-News-SadG";
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,12 +28,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("lodgelink");
+    const propertyCollection = db.collection("properties");
+    // const bookingCollection = db.collection("propertiesBooked");
+
+    app.get('/properties', async (req, res) => {
+      const result = await propertyCollection.find().toArray();
+      res.json(result);
+    })
+
+    app.post('/properties', async (req, res) => {
+      const propertyData = req.body;
+      const result = await propertyCollection.insertOne(propertyData);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
